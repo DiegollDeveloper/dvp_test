f1740bfd2bdf3750f6fad2186bdcd733b31fd157
Diego Gonzalez <diego18_98@hotmail.com>
2023-03-10 18:11:36 -0500

    Remove unused exception

--- lib/core/errors/exceptions.dart
+++ lib/core/errors/exceptions.dart
@@ -1,9 +1,3 @@
-class SearchPlaceException implements Exception {
-  final String message;
-
-  SearchPlaceException({required this.message});
-}
-
 class LoginExeption implements Exception {
   final String message;
 
